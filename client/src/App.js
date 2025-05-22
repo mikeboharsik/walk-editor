54c7e317e3187fcddd3291ae078f913df11dc167
Mike Boharsik <mboharsik@gmail.com>
2025-05-21 22:08:30 -0400

    render date components in a grid

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,33 @@ import { useCallback, useEffect, useState } from 'react';
 
 const baseUrl = 'http://localhost:8089';
 
+const optionsPerRow = 5;
+
 function SelectDateComponent({ isVisible, options, updateValue, revert }) {
   if (isVisible && options) {
+    const sortedOptions = options.toSorted((a,b) => parseInt(a) < parseInt(b) ? -1 : parseInt(a) > parseInt(b) ? 1 : 0);
+    const sections = [];
+    const sectionCount = Math.ceil(sortedOptions.length / optionsPerRow);
+    for (let i = 0; i < sectionCount; i++) {
+      sections.push(sortedOptions.slice(i * optionsPerRow, (i * optionsPerRow) + optionsPerRow).map(component => {
+        return <div key={`year-${component}`} style={{ cursor: 'pointer' }} onClick={() => updateValue(component)}>{component}</div>
+      }));
+    }
+
     return (
-      <div>
-        {revert && <span style={{ cursor: 'pointer' }} onClick={revert}>{'←'}</span>}
-        {
-          options
-            .toSorted((a,b) => parseInt(a) < parseInt(b) ? -1 : parseInt(a) > parseInt(b) ? 1 : 0)
-            .map(dateComponent => (
-              <div key={`year-${dateComponent}`} style={{ cursor: 'pointer' }} onClick={() => updateValue(dateComponent)}>{dateComponent}</div>
-            ))
-        }
-      </div>);
+      <>
+        {revert && <span style={{ cursor: 'pointer', marginBottom: '1em' }} onClick={revert}>{'←'}</span>}
+        <div style={{ width: '30%' }}>
+          {
+            sections
+              .map((section) => (
+                <div style={{ display: 'flex', justifyContent: 'space-evenly', marginBottom: '1em' }}>
+                  {section}
+                </div>
+              ))
+          }
+        </div>
+      </>);
   }
 
   return null;
